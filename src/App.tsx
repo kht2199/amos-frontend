14e5cba3bb6c875184ec9ebb13e2340f023e893e
heetaek.kim <kht2199@gmail.com>
2026-03-18 15:27:26 +0900

    refactor: 라우트 코드를 App.tsx에서 src/router/Route.tsx로 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,14 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router";
+import AppRoutes from "./router/Route";
 
 const queryClient = new QueryClient();
-
-import DefaultLayout from "./layouts/DefaultLayout";
-import AlertHistoryPage from "./pages/alert-history/AlertHistoryPage";
-import AlertSettingPage from "./pages/alert-setting/AlertSettingPage";
-import ContactManagementPage from "./pages/contact-management/ContactManagementPage";
-import LlmPage from "./pages/llm/LlmPage";
-import MainPage from "./pages/main/MainPage";
-import BridgeMonitoringPage from "./pages/monitoring/BridgeMonitoringPage";
-import FabCustomMonitoringPage from "./pages/monitoring/FabCustomMonitoringPage";
-import FabLayoutMonitoringPage from "./pages/monitoring/FabLayoutMonitoringPage";
-import ServerMonitoringPage from "./pages/monitoring/ServerMonitoringPage";
-import AmChartsSamplePage from "./pages/samples/AmChartsSamplePage";
-import EChartsSamplePage from "./pages/samples/EChartsSamplePage";
 
 export default function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<BrowserRouter>
-				<Routes>
-					<Route element={<DefaultLayout />}>
-						<Route index element={<MainPage />} />
-						<Route path="monitor">
-							<Route index element={<Navigate to="fab-layout" replace />} />
-							<Route path="fab-layout" element={<FabLayoutMonitoringPage />} />
-							<Route path="bridge" element={<BridgeMonitoringPage />} />
-							<Route path="fab-custom" element={<FabCustomMonitoringPage />} />
-							<Route path="server" element={<ServerMonitoringPage />} />
-						</Route>
-						<Route path="history/alert" element={<AlertHistoryPage />} />
-						<Route path="llm" element={<LlmPage />} />
-						<Route path="manage/contact" element={<ContactManagementPage />} />
-						<Route path="settings/alert" element={<AlertSettingPage />} />
-						<Route path="sample/echarts" element={<EChartsSamplePage />} />
-						<Route path="sample/amcharts" element={<AmChartsSamplePage />} />
-					</Route>
-				</Routes>
+				<AppRoutes />
 			</BrowserRouter>
 		</QueryClientProvider>
 	);
---
+++ src/router/Route.tsx
@@ -0,0 +1,36 @@
+import { Navigate, Route, Routes } from "react-router";
+import DefaultLayout from "../layouts/DefaultLayout";
+import AlertHistoryPage from "../pages/alert-history/AlertHistoryPage";
+import AlertSettingPage from "../pages/alert-setting/AlertSettingPage";
+import ContactManagementPage from "../pages/contact-management/ContactManagementPage";
+import LlmPage from "../pages/llm/LlmPage";
+import MainPage from "../pages/main/MainPage";
+import BridgeMonitoringPage from "../pages/monitoring/BridgeMonitoringPage";
+import FabCustomMonitoringPage from "../pages/monitoring/FabCustomMonitoringPage";
+import FabLayoutMonitoringPage from "../pages/monitoring/FabLayoutMonitoringPage";
+import ServerMonitoringPage from "../pages/monitoring/ServerMonitoringPage";
+import AmChartsSamplePage from "../pages/samples/AmChartsSamplePage";
+import EChartsSamplePage from "../pages/samples/EChartsSamplePage";
+
+export default function AppRoutes() {
+	return (
+		<Routes>
+			<Route element={<DefaultLayout />}>
+				<Route index element={<MainPage />} />
+				<Route path="monitor">
+					<Route index element={<Navigate to="fab-layout" replace />} />
+					<Route path="fab-layout" element={<FabLayoutMonitoringPage />} />
+					<Route path="bridge" element={<BridgeMonitoringPage />} />
+					<Route path="fab-custom" element={<FabCustomMonitoringPage />} />
+					<Route path="server" element={<ServerMonitoringPage />} />
+				</Route>
+				<Route path="history/alert" element={<AlertHistoryPage />} />
+				<Route path="llm" element={<LlmPage />} />
+				<Route path="manage/contact" element={<ContactManagementPage />} />
+				<Route path="settings/alert" element={<AlertSettingPage />} />
+				<Route path="sample/echarts" element={<EChartsSamplePage />} />
+				<Route path="sample/amcharts" element={<AmChartsSamplePage />} />
+			</Route>
+		</Routes>
+	);
+}
