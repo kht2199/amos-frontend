c0e8cb9091757c552595d18ecdf0bc21eac0d2ed
heetaek.kim <kht2199@gmail.com>
2026-03-18 15:24:41 +0900

    feat: react-i18next 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./lib/i18n";
 
 setConfig({
 	baseURL: import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8080",
